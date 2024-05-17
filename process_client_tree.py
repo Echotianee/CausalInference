d4eabdd5ebec228edfa4c89a4bd85b96fdd12c56
mehdigreefhorst <mehdigreefhorst@gmail.com>
2024-05-17 23:30:53 +0200

    Made the code to add new columns: - weather data - total amount purchased of that product until that point in time - total amount purchased of that product category until that point in time

--- process_client_tree.py
+++ process_client_tree.py
@@ -1,12 +1,15 @@
 import datetime
 import os
+
+import numpy as np
 import pandas as pd
 import random
 import multiprocessing
 
 
 class Client:
-    def __init__(self, client_id, client_folder_path):
+    def __init__(self, client_id,
+                 client_folder_path: str = "client_data"):
         self.client_id = client_id
         self.client_purchases_table_path = os.path.join(client_folder_path, self.client_id, "purchases.csv")
         self.client_events_table_path = os.path.join(client_folder_path, self.client_id, "events.csv")
@@ -35,6 +38,122 @@ class Client:
 
             self.client_events_table.loc[index, 'purchases_7_day_after'] = len(purchases_7_day_after)
             self.client_events_table.loc[index, 'purchases_30_day_after'] = len(purchases_30_day_after)
+
+    def process_client_add_total_spend_on_product(self):
+        """
+        This function will process the client and add the total_spend_on_product column to the events.csv file
+        Returns:
+
+        """
+
+        for index, row in self.client_events_table.iterrows():
+            user_id = row["USER_CLIENT_NUMBER"]
+            date = row["DATE"]
+            proposition_id = row["PROPOSITION"]
+            # Replace these with your actual function to calculate events prior to purchase date
+
+            self.client_events_table.loc[index, 'total_spend_on_product'] = self.total_amount_spend_on_product(
+                proposition_id, date, user_id)
+
+    def total_amount_spend_on_product(self, proposition_id, date, user_id):
+        """
+        This function will calculate the total amount spend on the product. before this date for the client
+        :param proposition_id:
+        :param date: until this date the total amount spend on the product will be calculated
+        :param user_id: the user id of the client
+        :return: total amount spend on the product
+        """
+        total_spend = 0
+        date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        product_purchase_events = self.client_purchases_table[
+            (self.client_purchases_table["USER_CLIENT_NUMBER"] == user_id) &
+            (self.client_purchases_table["DATE"] <= str(date)) &
+            (self.client_purchases_table["PROPOSITION"] == proposition_id)
+
+            ]
+        total_spend = product_purchase_events["AMOUNT"].sum()
+        return total_spend
+
+    def setup_product_category(self):
+        """
+        this functions sets up the product category for the puchases table of each proposition id
+        Returns:
+
+        """
+        self.client_purchases_table["ARTICLE_CATEGORIE"] = 0
+        for proposition_id in self.client_purchases_table["PROPOSITION"].unique():
+            product_category = self.get_product_category(proposition_id)
+            #print(product_category)
+            #print(self.client_purchases_table[self.client_purchases_table["PROPOSITION"]==proposition_id].to_html())
+
+            for index, row in self.client_purchases_table[self.client_purchases_table["PROPOSITION"]==proposition_id].iterrows():
+                row["ARTICLE_CATEGORIE"] = product_category
+
+
+
+    def get_product_category(self, proposition_id):
+        """
+        This function will get the product category of the proposition id
+        :param proposition_id:
+        :return: the product category of the proposition id
+        """
+        all_product_rows_df = self.client_events_table[self.client_events_table["PROPOSITION"] == proposition_id]
+        try:
+
+            article_category = all_product_rows_df["ARTICLE_CATEGORIE"].values[0]
+
+        except IndexError:
+            article_category = np.nan
+
+        return article_category
+
+    def process_client_add_total_spend_on_category_product(self):
+        """
+        This function will process the client and add the total_spend_on_category_product column to the events.csv file
+        Returns:
+
+        """
+
+        for index, row in self.client_events_table.iterrows():
+            user_id = row["USER_CLIENT_NUMBER"]
+            date = row["DATE"]
+            article_category = row["ARTICLE_CATEGORIE"]
+            # Replace these with your actual function to calculate events prior to purchase date
+
+            self.client_events_table.loc[
+                index, 'total_spend_on_category_product'] = self.total_amount_spend_on_category_product(
+                article_category, date)
+
+    def total_amount_spend_on_category_product(self, article_category, date):
+        """
+        This function will calculate the total amount spend on the product. before this date for the client
+        :param article_category: the category of the product
+        :param date: until this date the total amount spend on the product will be calculated
+        :param user_id: the user id of the client
+        :return: total amount spend on the product
+        :TODO: remove the product_id_finder and make the file already have the product category
+        """
+        total_spend = 0
+        date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+
+        bought_product_id_in_category = self.client_purchases_table[
+            (self.client_purchases_table["ARTICLE_CATEGORIE"] == article_category)
+        ]["PROPOSITION"].unique()
+
+        product_purchase_events = self.client_purchases_table[
+            (self.client_purchases_table["DATE"] <= str(date)) &
+            (self.client_purchases_table["PROPOSITION"].isin(bought_product_id_in_category))
+
+            ]
+        total_spend = product_purchase_events["AMOUNT"].sum()
+        return total_spend
+
+    def write_tables_to_csv(self):
+        """
+        This function will write the tables to the csv
+        :return:
+        """
+        self.client_purchases_table.to_csv(self.client_purchases_table_path, sep="|", index=False)
         self.client_events_table.to_csv(self.client_events_table_path, sep="|", index=False)
 
     @staticmethod
@@ -42,9 +161,9 @@ class Client:
                                  testing=False):
         """
         it gets all the events from the specific date for one day before and after the given date
-        :param data_events:
+        :param purchase_events:
         :param user_id:
-        :param date: 2024-11-18
+        :param purchase_date: 2024-11-18
         :param date_range: 30 number of dates to look after the event date
         :param testing: if True, it will print the number of events found
         :return: all events previous to the purchase date
@@ -97,18 +216,34 @@ class ProcessClientsFolderTree:
             else:
                 print("Skipping file: ", client_id)
 
-
-    def process_clients_in_chunk(self, client_id_chunk):
+    def process_clients_in_chunk_7days(self, client_id_chunk, add_purchases=True,
+                                       add_total_product_spend=False,
+                                       add_total_category_product_spend=False):
         """
-        This function will process the clients in the chunk
+        This function will process the clients in the chunk and add the purchases_7_day_after and purchases_30_day_after
         :param client_id_chunk:
+        :param add_purchases: if True, it will add the purchases_7_day_after and purchases_30_day_after columns
+        :param add_total_product_spend: if True, it will add the total_spend_on_product column
+        :param add_total_category_product_spend: if True, it will add the total_spend_on_category_product column
+
         :return:
         """
         for index, client_id in enumerate(client_id_chunk):
             if index % 10 == 0:
                 print("Processing client: ", client_id)
-            client = Client(client_id=client_id, client_folder_path= "client_data")
-            client.process_client_add_purchase_nr_to_event_write_to_csv()
+            client = Client(client_id=client_id, client_folder_path="client_data")
+            if add_purchases:
+                client.process_client_add_purchase_nr_to_event_write_to_csv()
+
+            if add_total_product_spend:
+                client.process_client_add_total_spend_on_product()
+
+            if add_total_category_product_spend:
+                client.process_client_add_total_spend_on_category_product()
+
+
+
+            client.write_tables_to_csv()
             print("Done with client: ", client_id)
 
     def setup_chunks_from_client_list(self, client_list, chunk_size=100):
@@ -119,17 +254,33 @@ class ProcessClientsFolderTree:
         chunks = [client_list[i:i + chunk_size] for i in range(0, len(client_list), chunk_size)]
         return chunks
 
-    def process_client_purchases_to_event_multiprocessing(self):
+    def process_client_purchases_to_event_multiprocessing(self, add_purchases=True,
+                                                          add_total_product_spend=False,
+                                                          add_total_category_product_spend=False):
         """
         This function will process the clients in the client folder and add the purchases_7_day_after
         and purchases_30_day_after columns to the events.csv file
+        :param add_purchases: if True, it will add the purchases_7_day_after and purchases_30_day_after columns
+        :param add_total_product_spend: if True, it will add the total_spend_on_product column
+        :param add_total_category_product_spend: if True, it will add the total_spend_on_category_product column
         :return:
         """
         chunks_to_process = self.setup_chunks_from_client_list(self.clientid_list, chunk_size=100)
 
-        with multiprocessing.Pool(processes=8) as pool:
+        args_for_processing = [
+            (chunk,
+             add_purchases,
+             add_total_product_spend,
+             add_total_category_product_spend
+             )
+            for chunk in chunks_to_process]
 
-            results = pool.map(self.process_clients_in_chunk, chunks_to_process)
+        with multiprocessing.Pool(processes=8) as pool:
+            results = pool.starmap(self.process_clients_in_chunk_7days, args_for_processing)
+
+        if add_purchases:
+            with multiprocessing.Pool(processes=8) as pool:
+                results = pool.map(self.process_clients_in_chunk_7days, chunks_to_process)
 
     #________________________________Aggregate with multiprocessing_________________________________________________#
 
@@ -141,16 +292,16 @@ class ProcessClientsFolderTree:
         :return:
         """
         if table_name == "purchases":
-            client = Client(client_id=client_id_chunk[0], client_folder_path= "client_data")
+            client = Client(client_id=client_id_chunk[0], client_folder_path="client_data")
             df = client.get_table_of_client(table_name)
             for index, client_id in enumerate(client_id_chunk[1:]):
-                client = Client(client_id=client_id, client_folder_path= "client_data")
+                client = Client(client_id=client_id, client_folder_path="client_data")
                 df = pd.concat([df, client.get_table_of_client(table_name)], ignore_index=True)
         elif table_name == "events":
-            client = Client(client_id=client_id_chunk[0], client_folder_path= "client_data")
+            client = Client(client_id=client_id_chunk[0], client_folder_path="client_data")
             df = client.get_table_of_client(table_name)
             for index, client_id in enumerate(client_id_chunk[1:]):
-                client = Client(client_id=client_id, client_folder_path= "client_data")
+                client = Client(client_id=client_id, client_folder_path="client_data")
                 df = pd.concat([df, client.get_table_of_client(table_name)], ignore_index=True)
         return df
 
@@ -180,27 +331,37 @@ class ProcessClientsFolderTree:
         return df
 
 
-
 if __name__ == "__main__":
     #if you run this program, you will add each of the clients in the client_data folder. THen for each of the
     # clients you will add the purchases_7_day_after and purchases_30_day_after columns to the events.csv file
+    if not os.path.exists("client_data/230/events.csv"):
+        raise Exception(
+            "you have to first setup the client tree structure with save_clients_in_folder_structure_updated.ipynb")
+        exit()
     process_clients = ProcessClientsFolderTree()
     client_id_list = process_clients.clientid_list
-    chunks_to_process = process_clients.setup_chunks_from_client_list(client_id_list, chunk_size=100)
+    chunks_to_process = process_clients.setup_chunks_from_client_list(client_id_list, chunk_size=100)[:8]
 
     # If you want to process the clients in the client folder and add the purchases_7_day_after
     # and purchases_30_day_after columns to the events.csv file, you can use the function below
 
-    #process_clients.process_client_purchases_to_event_multiprocessing()
+    process_clients.process_client_purchases_to_event_multiprocessing(
+        add_purchases=False,
+        add_total_product_spend=True,
+        add_total_category_product_spend=True)
+    """process_clients.process_clients_in_chunk_7days(chunks_to_process[0],
+                                                   add_purchases=False,
+                                                   add_total_product_spend=False,
+                                                   add_total_category_product_spend=True,
 
+                                                   )"""
 
 
     # If you want to aggregate the clients in the client folder using multiprocessing, you can use the function below
     # This function will aggregate the clients in the client folder using multiprocessing
 
-    process_clients.aggregate_clients_multi_processing(table_name="purchases", write_to_csv=True,
-                                                       file_name_to_write="processed_data/processed_purchase_events.csv")
+    #process_clients.aggregate_clients_multi_processing(table_name="purchases", write_to_csv=True,
+    #                                                   file_name_to_write="processed_data/processed_purchase_events_final.csv")
 
-    process_clients.aggregate_clients_multi_processing(table_name="events", write_to_csv=True,
-                                                       file_name_to_write="processed_data/processed_events.csv")
-
+    #process_clients.aggregate_clients_multi_processing(table_name="events", write_to_csv=True,
+    #                                                   file_name_to_write="processed_data/processed_events_final.csv")
